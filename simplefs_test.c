5ffbcc13d2dc0569580b1bda5c76a5c26ef6cd5d
Nick1296 <mattianicolella@gmail.com>
2018-02-24 18:52:06 +0100

    fixed a bug into disk_test function in test module

--- simplefs_test.c
+++ simplefs_test.c
@@ -40,15 +40,15 @@ void driver_test(DiskDriver *disk){
 
   printf("\t testing disk_write\n");
   char *v = "Test di scrittura su settore del nostro bellissimo disk_driver, devo scrivere tanto per provare il corretto funzionamento. hjefgkjhgfwsgfkgfwsejkfgshfglsujgfjkhgfjushegjkhdsgfjlhsgfvjlhdfjgyflyfgaelfialrgigsdiufgdriyihulkfuilfuhwlidiujelkifudlgdfjlvshgdshfgsdahjsfgsuyhgkjuddsdydsgflysdfydsgflydgljsgjhshgvfdljhgslhkgdkfvglhgsdlhfghdslgsajhbsdjvfgshfsdhvgsdfklgsjdgfsdhubggfjsdfgsdhjfvgsjgfgsdjhfgjsahfdfgsdilvcfsdgfvckhdsvgcbsdghfgslhcvlhgfdsjhbfhfgdsxlflvkhfgdfhsdbglgdflhegfjlhsagflhjegbfls<dgderlhfgbvsl Test";
-  int result = DiskDriver_writeBlock(&disk, v, 0);
+  int result = DiskDriver_writeBlock(disk, v, 0);
   printf("disk_write result: %d\n", result);
   printf("\t testing disk_read\n");
   char data[512];
-  result = DiskDriver_readBlock(&disk, data, 0);
+  result = DiskDriver_readBlock(disk, data, 0);
   printf("disk_read result: %d\n", result);
   printf("Data readed->%s<-\nend\n\n",data);
   printf("\t testing disk_flush\n");
-  result = DiskDriver_flush(&disk);
+  result = DiskDriver_flush(disk);
   printf("flush result: %d\n", result);
 }
 
